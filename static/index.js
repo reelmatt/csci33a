2a82a44e822203bf45d255ac55297780bc732e32
Matt Thomas <matthew_thomas@hms.harvard.edu>
2019-07-18 20:51:56 -0400

    Factored out page into parts; javascript too

--- static/index.js
+++ static/index.js
@@ -1,79 +1,51 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    localStorage.removeItem('channel')
     const message_template = Handlebars.compile(document.querySelector('#message').innerHTML);
+    const channel_template = Handlebars.compile(document.querySelector('#channel').innerHTML);
 
-    // If no user stored locally, present form for them to register
-    if (!localStorage.getItem('user')) {
-        console.log("no users...");
-        document.querySelector('#enterDisplayName').style.display = "inherit";
-
-        // By default, submit button is disabled
-        document.querySelector('#displayNameSubmit').disabled = true;
-
-        // Enable button only if there is text in the input field
-        document.querySelector('#displayName').onkeyup = () => {
-            if (document.querySelector('#displayName').value.length > 0)
-                document.querySelector('#displayNameSubmit').disabled = false;
-            else
-                document.querySelector('#displayNameSubmit').disabled = true;
-        };
-
-        document.querySelector('#displayNameForm').onsubmit = () => {
-            console.log("Showing up here...");
-
-            let name = document.querySelector("#displayName").value;
-
-            localStorage.setItem('user', name);
-
-            console.log(`Entered display name of ${name}`);
-            document.querySelector('#enterDisplayName').remove();
+    let channel = localStorage.getItem('channel');
 
 
-            // Stop form from submitting
-            return false;
-        };
-    } else {
-        console.log(`Any users? ${localStorage.getItem('displayName')}`);
-        document.querySelector('#enterDisplayName').remove();
-    }
 
     document.getElementById('channelError').style.display = "none";
-
 
     // Connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
+
+    /*******************************
+     * Create and switch channels
+     *******************************/
     // When connected, configure buttons
     socket.on('connect', () => {
+        if (channel) {
+            socket.emit('load channel', channel);
+        }
         document.querySelector('#channelNameForm').onsubmit = () => {
             const channel = document.getElementById('channelName').value;
 
             socket.emit('create channel', {'name': channel});
 
             document.getElementById('channelError').style.display = "none";
-            document.getElementById('channelError').innerText = '';
-            document.getElementById('channelName').value = '';
+
+            clearInput('channelName');
             return false;
         };
 
         document.querySelector('#messageForm').onsubmit = () => {
                 const text = document.getElementById('messageContents').value;
-                const now = new Date();
-                socket.emit('send message', {
-                    'text': text,
-                    'user': localStorage.getItem('user'),
-                    'time': now.toLocaleString('en-US', {timeZone: 'EST'}),
-                    'channel': localStorage.getItem('channel')
-                });
-                document.getElementById('messageContents').value = '';
+
+                socket.emit('send message', constructMessage(text));
+                clearInput('messageContents');
                 return false;
         };
 
-        // Each button should emit a "submit vote" event
+        // For any channels previously added, make button change display
         document.querySelectorAll('button.channel').forEach(button => {
             console.log("button name is " + button.name);
-            button.onclick = () => {
+            // button.onclick = loadChannel(button.name);
+           button.onclick = () => {
+                console.log("OTHER onclick");
                 localStorage.setItem('channel', button.name);
                 document.getElementById('channelHead').innerText = button.name;
                 socket.emit('load channel', button.name);
@@ -92,56 +64,95 @@ document.addEventListener('DOMContentLoaded', () => {
             if (message.channel !== localStorage.getItem('channel')) {
                 return;
             }
-            const temp = message_template({'message': message});
-            console.log(temp);
-            // const li = document.createElement('li');
-            // li.innerHTML = message.text + ' sent by ' + message.user + ' at ' + message.time;
-            document.querySelector("#messageList").innerHTML += temp;
+            document.querySelector("#messageList").innerHTML += message_template({'message': message});
     });
 
     socket.on('load messages', data => {
         console.log("Loading messages....");
-
-
-        // localStorage.setItem('channel', data.channel);
+        document.getElementById('channelHead').innerText = localStorage.getItem('channel');
         document.getElementById('messageList').innerHTML = '';
         let list = document.getElementById('messageList');
         console.log(data["messages"]);
         if (data["messages"]) {
             data["messages"].forEach(message => {
-                const temp = message_template({'message': data});
-                console.log(temp);
-                // const li = document.createElement('li');
-                // li.innerHTML = message.text + ' sent by ' + message.user + ' at ' + message.time;
-                list.innerHTML += temp;
+                list.innerHTML += message_template({'message': message});
             });
         }
 
     });
 
-    socket.on('channel list', data => {
-        console.log("Appending to channel list...");
-        console.log(data.channel);
-        document.getElementById('channelHead').innerText = data.channel;
-        localStorage.setItem('channel', data.channel);
+    socket.on('channel list', channel => {
+/*
+        document.getElementById('channelHead').innerText = channel;
+        localStorage.setItem('channel', channel);
+
+        // let list = document.getElementById('channels');
+        //
+        // const li = document.createElement('li');
+        document.getElementById('channels').innerHTML += channel_template({'channel': channel});
+        // const button = channel_template({'channel': channel});
+        // console.log(button);
+        // const button = document.createElement('button');
+        // document.getElementById('messageList').innerHTML = '';
+        // button.setAttribute('class', 'channel');
+        // button.innerText = channel;
+        let button = document.querySelector(`button[name="${channel}"]`);
+        console.log(button);
+
+
+        button.onclick = () => {
+            console.log("ON CLICK TRIGGER!!");
+            document.getElementById('channelHead').innerHTML = channel;
+            localStorage.setItem('channel', channel);
+            socket.emit('load channel', channel);
+        };
+        // li.innerText = button;
+        // li.appendChild(button);
+        // console.log(li);
+        // list.innerHTML += button;
+        // document.querySelector("#channels").append(li);
+    });
+*/
+
+        document.getElementById('channelHead').innerText = channel;
+        localStorage.setItem('channel', channel);
         const li = document.createElement('li');
         const button = document.createElement('button');
         document.getElementById('messageList').innerHTML = '';
         button.setAttribute('class', 'channel');
-        button.innerText = data.channel;
+        button.innerText = channel;
+        // button.onclick = loadChannel(channel);
         button.onclick = () => {
             console.log("ON CLICK TRIGGER!!");
-            document.getElementById('channelHead').innerHTML = data.channel;
-            localStorage.setItem('channel', data.channel);
-            socket.emit('load channel', data.channel);
+            document.getElementById('channelHead').innerHTML = channel;
+            localStorage.setItem('channel', channel);
+            socket.emit('load channel', channel);
         };
 
         li.appendChild(button);
         console.log(li);
         document.querySelector("#channels").append(li);
     });
-
-
-
-
 });
+
+function loadChannel (channel) {
+    console.log("ON CLICK TRIGGER!!");
+    document.getElementById('channelHead').innerHTML = channel;
+    localStorage.setItem('channel', channel);
+    socket.emit('load channel', channel);
+};
+
+function constructMessage(text) {
+    const now = new Date();
+
+    return {
+        'text': text,
+        'user': localStorage.getItem('user'),
+        'time': now.toLocaleString('en-US', {timeZone: 'EST'}),
+        'channel': localStorage.getItem('channel')
+    };
+};
+
+function clearInput(element) {
+    document.getElementById(element).value = '';
+};
