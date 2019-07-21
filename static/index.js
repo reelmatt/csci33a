e955b633f573892b5f4faada0297674ccad24dc3
Matthew Thomas <mthomas@Qarth.local>
2019-07-21 08:09:08 -0400

    Change error display logic; style tweaks

--- static/index.js
+++ static/index.js
@@ -2,16 +2,24 @@
 var socket;
 
 document.addEventListener('DOMContentLoaded', () => {
+    // Hide any error messages
+    safeRemove('channelError');
+    safeRemove('messageError');
 
+    // Added Handlebars helper to check equality in if statement
+    // Code snippet copied from this Stack Overflow post
+    // https://stackoverflow.com/a/51976315
+    Handlebars.registerHelper('ifeq', function (a, b, options) {
+        if (a == b) { return options.fn(this); }
+        return options.inverse(this);
+    });
+
+    // Handlebars templates for message and channel
     const message_template = Handlebars.compile(document.querySelector('#message').innerHTML);
     const channel_template = Handlebars.compile(document.querySelector('#channel').innerHTML);
 
+    // If there's a local channel, remove it to start fresh
     let channel = localStorage.removeItem('channel');
-    // console.log("Do we have a local channel?");
-    // console.log(channel);
-
-    document.getElementById('channelError').style.display = "none";
-    document.getElementById('messageError').style.display = "none";
 
     // Connect to websocket
     socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
@@ -22,8 +30,6 @@ document.addEventListener('DOMContentLoaded', () => {
     socket.on('connect', () => {
         // Form to create new channel; 'channel error' is emitted if a duplicate
         document.querySelector('#channelNameForm').onsubmit = () => {
-            // Clear any previous errors
-            document.getElementById('channelError').style.display = "none";
 
             // Check channel name for duplicates
             const channel = document.getElementById('channelName').value;
@@ -36,16 +42,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // Form to send messages
         document.querySelector('#messageForm').onsubmit = () => {
-            // Clear any previous errors
-            document.getElementById('messageError').style.display = "none";
-
-            // Get the message, add user/timestamp, and add to Flask memory
+            // Get the message text, add user, timestamp, and channel
             const message = constructMessage();
 
-            // Clear input field and prevent form from submitting
+            // If invalid, constructMessage() will return null
             if (message) {
+                // Good to go, store in server and clear input
                 socket.emit('send message', message);
                 clearInput('messageContents');
+
+                // Clear any previous errors
+                safeRemove('messageError');
             } else {
                 showErrorMessage('messageError', 'Please select a channel to send messages to.');
             }
@@ -63,51 +70,52 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // A message was sent by some user
     socket.on('receive message', message => {
-        // If it does not belong to the current channel, do not add it
+        // If it does not belong to the current channel, do nothing
         if (message.channel !== localStorage.getItem('channel')) {
             return;
         }
 
         // Belongs to the current channel, display the message
-        addNewContent('messageList', message_template({'message': message}));
+        addNewContent('messageList', message_template({'message': message, 'localUser': localStorage.getItem('user')}));
     });
 
+    // A message was deleted by its sender
     socket.on('remove message', messageToDelete => {
-      // If it does not belong to the current channel, do not add it
+      // If it does not belong to the current channel, do nothing
       if (messageToDelete.channel !== localStorage.getItem('channel')) {
           return;
       }
 
-      console.log("REMOVE THE MESSAGE");
-
+      // Get the messages currently displayed on the screen
       const allMessages = document.querySelectorAll('#messageList > li');
-      // console.log(allMessages[0]);
 
+      // Search to find the one that matches
       allMessages.forEach(message => {
-          console.log(message);
-            const text = message.querySelector('.card-text').innerText;
-            const user = message.querySelector('.card-title').innerText;
-            const time = message.querySelector('.card-subtitle').innerText;;
+          const text = message.querySelector('.card-text').innerText;
+          const user = message.querySelector('.card-title').innerText;
+          const time = message.querySelector('.card-subtitle').innerText;;
 
-            if (text === messageToDelete.text && user === messageToDelete.user && time === messageToDelete.time) {
-                console.log("MESSAGES MATCH!!!!");
-                message.remove();
-                return;
-            } else {
-              console.log("NO MATCH!");
-            }
+          // Check the parts to see if they match
+          if (text === messageToDelete.text && user === messageToDelete.user &&
+              time === messageToDelete.time) {
+              // remove it from the screen and stop looking
+              message.remove();
+              return;
+          }
       });
     });
 
     // When loading a channel, also load any existing messages
     socket.on('load messages', messages => {
         messages.forEach(message => {
-            addNewContent('messageList', message_template({'message': message}));
+            addNewContent('messageList', message_template({'message': message, 'localUser': localStorage.getItem('user')}));
         });
     });
 
     // When a valid new channel is entered, add it to the list
     socket.on('list channel', channel => {
+      // Clear any previous errors
+      safeRemove('channelError');
         addNewContent('channels', channel_template({'channel': channel}));
 
         let notification = document.querySelector('#channels > p');
@@ -119,32 +127,25 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 document.addEventListener('click', e => {
-    // console.log("EVENT LISTENER");
-    console.log(e.target.className);
-    // console.log(e.target [.class, .tagName, .name, .type]);
+    // Only add listeners on buttons
     if (e.target.type === "button") {
         const className = e.target.className;
+
+        // Buttons to change channels
         if (className.search('channel') !== -1) {
-            console.log("SUCCESS!!!!");
-            console.log(className.search('channel'));
             const channel = e.target.name;
-            // socket.emit('load channel', channel);
             changeChannel(channel);
             return false;
-            // document.getElementById('channelHead').innerText = "#" + e.target.name;
-        } else if (className.search('deleteMessage') !== -1) {
-            console.log("DELETE MESSAGE");
+        }
+        // Buttons to delete messages
+        else if (className.search('deleteMessage') !== -1) {
             const message = e.target.parentNode;
-            console.log(message);
             const messageSender = message.querySelector('.card-title').innerText;
-            console.log("SENDER: ");
-            console.log(messageSender);
             const user = localStorage.getItem('user');
 
-
+            // If the messages was sent by the current user, okay to delete
             if (messageSender === user) {
-                console.log("ALLOWED TO DELETE. EMIT MESSAGE");
-
+                // Extract the message information
                 const thisMessage = {
                   'text': message.querySelector('.card-text').innerText,
                   'user': messageSender,
@@ -152,24 +153,27 @@ document.addEventListener('click', e => {
                   'channel': localStorage.getItem('channel')
                 };
 
-                console.log(thisMessage);
+                // Send a notification to server
                 socket.emit('delete message', thisMessage);
             }
         }
-
     }
-
 });
 
+// Add new content (message or channel name) to the appropriate part of the page
 function addNewContent(elementId, content) {
     document.getElementById(elementId).innerHTML += content;
 };
 
+// Should an error occur, generate the message and display in the appropriate
+// section of the page
 function showErrorMessage(elementId, message) {
-    document.getElementById(elementId).innerText = message;
-    document.getElementById(elementId).style.display = "inherit";
+    const error_template = Handlebars.compile(document.querySelector('#errorMessage').innerHTML);
+    document.getElementById(elementId).innerHTML = error_template({'message': message});
 };
 
+// When the user changes a channel, update localStorage, change the page's
+// header, and blank out the messages, to prepare for loading any stored messages
 function changeChannel(channel) {
     localStorage.setItem('channel', channel);
     document.getElementById('channelHead').innerText = "#" + channel;
@@ -177,15 +181,19 @@ function changeChannel(channel) {
     socket.emit('load channel', channel);
 };
 
+// Perform some basic error-checking before creating a message Object to pass
+// through to the application.
 function constructMessage() {
-    console.log("IN constructMessage()");
-
+    // If a channel hasn't been selected, stop message from sending
     if ( ! localStorage.getItem('channel')) {
         return null;
     }
+
+    // Otherwise, get the message contents and timestamp
     const text = document.getElementById('messageContents').value;
     const now = new Date();
 
+    // Create the message Object
     return {
         'text': text,
         'user': localStorage.getItem('user'),
@@ -194,6 +202,17 @@ function constructMessage() {
     };
 };
 
+// Helper function to clear the text input field
 function clearInput(element) {
     document.getElementById(element).value = '';
 };
+
+// Helper function to safely remove an element from the screen, only if such
+// an element exists. (Prevents console errors should element *not* exist.)
+function safeRemove(elementId) {
+  let toRemove = document.getElementById(elementId).firstElementChild;
+
+  if (toRemove) {
+    toRemove.remove();
+  }
+}
