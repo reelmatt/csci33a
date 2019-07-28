37ec67315511bf1ff3b54960952c5e399d38fa4d
Matt Thomas <matthew_thomas@hms.harvard.edu>
2019-07-28 09:15:55 -0400

    Add friendly_status to Status model

--- orders/models.py
+++ orders/models.py
@@ -38,9 +38,10 @@ class Item(models.Model):
 # An order's status
 class Status(models.Model):
     status = models.CharField(max_length=32)
+    friendly_status = models.CharField(max_length=32)
 
     def __str__(self):
-        return f"{self.status}"
+        return f"{self.friendly_status}"
 
 
 # A customer's order
