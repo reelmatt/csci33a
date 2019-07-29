6760710ae1328e449a8ba11ee64fa11678de1ce3
Matt Thomas <matthew_thomas@hms.harvard.edu>
2019-07-29 09:55:31 -0400

    Add personal touch, admin update status of orders

--- orders/urls.py
+++ orders/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("cart", views.cart, name="cart"),
     path("checkout", views.checkout, name="checkout"),
     path("orders/<int:order_id>", views.order, name="order"),
+    path("orders/<int:order_id>/update", views.update_order_status, name="update_order_status"),
     path("orders", views.view_orders, name="view_orders"),
     path("menu", views.menu, name="menu"),
     path("menu/<int:item_id>", views.item, name="item"),
