48fe6a4dd2cdb6491379bf4d3c3c8f5542b64f0f
Matthew Thomas <mthomas@Qarth.local>
2019-07-30 20:02:25 -0400

    Customize admin interface for topping selection

--- orders/admin.py
+++ orders/admin.py
@@ -3,22 +3,22 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from .models import Category, Order, Topping, Item, Status, Size, CartItem
 
-# Register your models here.
-# class ItemInline(admin.StackedInline):
-#     model = Item.toppings.through
-#
-# class ToppingAdmin(admin.ModelAdmin):
-#     inlines = [ItemInline]
-#
-# class ItemAdmin(admin.ModelAdmin):
-#     filter_horizontal = ("toppings",)
-#
-#
+# Admin customization modified from the 'airline4' example in code for
+# lecture 8
+class ToppingInline(admin.StackedInline):
+    model = Topping.allowed_on.through
+    extra = 1
+
+class ItemAdmin(admin.ModelAdmin):
+    inlines = [ToppingInline]
+
+class ToppingAdmin(admin.ModelAdmin):
+    filter_horizontal = ("allowed_on",)
 
 admin.site.register(Category)
 admin.site.register(Size)
 admin.site.register(Order)
-admin.site.register(Topping)
-admin.site.register(Item)
+admin.site.register(Topping, ToppingAdmin)
+admin.site.register(Item, ItemAdmin)
 admin.site.register(CartItem)
 admin.site.register(Status)
