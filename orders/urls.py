9297d04150121e4cdbf632bd5cae5da112f5cb46
Matthew Thomas <mthomas@Qarth.local>
2019-07-30 20:07:11 -0400

    Remove unused paths, organize others

--- orders/urls.py
+++ orders/urls.py
@@ -5,19 +5,14 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("", include("django.contrib.auth.urls")),
+    path("add_to_cart", views.add_to_cart, name="add_to_cart"),
     path("cart", views.cart, name="cart"),
     path("checkout", views.checkout, name="checkout"),
+    path("orders", views.view_orders, name="view_orders"),
     path("orders/<int:order_id>", views.order, name="order"),
     path("orders/<int:order_id>/update", views.update_order_status, name="update_order_status"),
-    path("orders", views.view_orders, name="view_orders"),
     path("menu", views.menu, name="menu"),
     path("menu/<int:item_id>", views.item, name="item"),
     path("register", views.register, name="register"),
-    path("add_to_cart", views.add_to_cart, name="add_to_cart"),
     path("remove_item", views.remove_item, name="remove_item")
-    # Replaced with auth.urls, see above
-    # path("login", views.login_view, name="login"),
-    # path("login", name="login"),
-    # path("logout", views.logout_view, name="logout")
-    # path("logout", name="logout")
 ]
