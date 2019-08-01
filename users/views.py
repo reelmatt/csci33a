977c36d5a71d0a2e20abc21814f696c9e3546b13
Matt Thomas <matthew_thomas@hms.harvard.edu>
2019-08-01 15:43:16 -0400

    Trying customizing auth forms

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+from .forms import UserCreationForm, AuthenticationForm
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/register.html"
+
+
+class LoginView(LoginView):
+    authentication_form = AuthenticationForm
