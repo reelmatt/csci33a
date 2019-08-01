e6fb500533cf1be453fd77f394d12e5327f2e655
Matt Thomas <matthew_thomas@hms.harvard.edu>
2019-08-01 17:43:28 -0400

    Change from sub-classed Views, to approach from project 3

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path, include
 from . import views
-from .views import LoginView
 
 urlpatterns = [
-    path('register', views.RegisterView.as_view(), name='register'),
-    path('login', LoginView.as_view(), name='login')
+    path('register', views.register, name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,15 +1,37 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.contrib.auth.views import LoginView
-from django.views.generic.edit import CreateView
+from django.urls import reverse
+from django.contrib.auth import login
+from django.contrib import messages
+from django.db import IntegrityError
 
-from .forms import UserCreationForm, AuthenticationForm
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from .models import User
 
-class RegisterView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy("login")
-    template_name = "registration/register.html"
+# Route to register new users
+def register(request):
+    # Regular request, display form
+    if request.method == "GET":
+        return render(request, "registration/register.html")
 
+    # If passwords don't match, return an error message
+    if request.POST["password"] != request.POST["confirmPassword"]:
+        messages.add_message(request, messages.ERROR, "Passwords don't match.")
+        return render(request, "registration/register.html")
 
-class LoginView(LoginView):
-    authentication_form = AuthenticationForm
+    # Otherwise, add user to database
+    try:
+        user = User.objects.create_user(
+            username=request.POST["username"],
+            email=request.POST["email"],
+            password=request.POST["password"],
+            last_name=request.POST["lastName"],
+            first_name=request.POST["firstName"]
+        )
+
+        # Log them in, and send to homepage
+        login(request, user)
+        return HttpResponseRedirect(reverse("home"))
+    # Username is not unique, throw error
+    except IntegrityError as e:
+        messages.add_message(request, messages.ERROR, "That username already exists.")
+        return render(request, "registration/register.html")
