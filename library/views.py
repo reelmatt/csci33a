aebbd0a43cef3672ffb620220840b684c3ee0f0b
Matthew Thomas <matthew_thomas@hms.harvard.edu>
2019-08-03 16:33:47 -0400

    Add placeholder views for library routes

--- library/views.py
+++ library/views.py
@@ -4,9 +4,14 @@ from django.contrib import messages
 from django.db import IntegrityError
 
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-from .models import Library, Book
 
 # Create your views here.
 # Route to register new users
 def library(request):
+    return render(request, "library/library.html")
+
+def edition(request):
+    return render(request, "library/library.html")
+
+def event(request):
     return render(request, "library/library.html")
