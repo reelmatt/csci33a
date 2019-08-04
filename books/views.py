27bcd3f066aab6d7ec8c938369cf826f652e41e2
Matthew Thomas <matthew_thomas@hms.harvard.edu>
2019-08-04 09:06:10 -0400

    Working code for book/acquire views

--- books/views.py
+++ books/views.py
@@ -1,7 +1,61 @@
+import os
+import requests
+from django.apps import apps
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.urls import reverse
 from django.shortcuts import render
+from library.models import Edition, Book, Library, Author, Publisher, Genre
+
 
 # Create your views here.
+def acquire(request, book_id):
+    # authors = Author.objects.filter()
+    # publisher = Publisher.Objects
+    # genre = Genre.objects
+    # book = Book.objects.create(
+    #     title =
+    #     publisher =
+    #     genre =
+    # )
+    #
+    # book.authors.set(authors)
+
+    print(f"\n\nIN ACQUIRE, id is {book_id}")
+    return HttpResponseRedirect(reverse("book", kwargs={"book_id": book_id}))
+
 
 def book(request, book_id):
     print(f"in book view, id is {book_id}")
-    return render(request, "books/book.html")
+
+    result = search_openlibrary(book_id)
+
+    # Access a dictionary item by index
+    # https://stackoverflow.com/questions/30362391/how-do-you-find-the-first-key-in-a-dictionary
+    key = list(result.keys())[0]
+
+
+
+
+    context = {
+        "book": result[key]
+    }
+    return render(request, "books/book.html", context)
+
+
+def search_openlibrary(book_id):
+    url = "http://openlibrary.org/api/books?"
+    key = f"ISBN:{book_id}"
+
+    res = requests.get(url, params={
+        "bibkeys": key,
+        "format": "json",
+        "jscmd": "data",
+    })
+
+    if res.status_code != 200:
+        raise Exception("ERROR: API request unsuccessful.")
+
+    # Parse response and extract info
+    data = res.json()
+
+    return data
--- search/views.py
+++ search/views.py
@@ -5,11 +5,6 @@ from django.shortcuts import render
 
 
 
-# Create your views here.
-def book(request, book_id):
-    print(f"in book view, id is {book_id}")
-    return render(request, "search/book.html")
-
 
 def search(request):
     if request.method == "GET":
