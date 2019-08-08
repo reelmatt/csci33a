177af6589171a5698d3311723c8d822dcf06eaca
Matthew Thomas <matthew_thomas@hms.harvard.edu>
2019-08-08 16:41:33 -0400

    Add urls/paths for stats route

--- library/urls.py
+++ library/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:edition_id>', views.edition, name='edition'),
     path('<int:edition_id>/<int:event_id>', views.event, name='event'),
     path('<int:edition_id>/post', views.status_update, name='status update'),
+    path('stats', views.stats, name='stats')
 ]
